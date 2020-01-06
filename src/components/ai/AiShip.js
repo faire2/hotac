6bedd4081cb647b6884c741a1e7f3fe1732db0bb
janda <rostislav.janda@fio.cz>
2020-01-06 12:32:43 +0100

    Added ShipActions and ShipTargetting components

--- src/components/ai/AiShip.js
+++ src/components/ai/AiShip.js
@@ -1,6 +1,8 @@
-import {Abi, Ships} from "../../data/Ships";
 import React, {useState} from "react";
 import {Button} from "react-bootstrap";
+
+import {Abi, Ships} from "../../data/Ships";
+import ShipActions, {TIELN} from "../../data/ShipActions";
 
 export function InnerShip(props) {
     const shipId = props.shipId;
@@ -9,15 +11,26 @@ export function InnerShip(props) {
     const [currShields, setCurrShields] = useState(maxShields);
     const [currHull, setCurrHull] = useState(maxHull);
 
-    function handleValueChange(e, isNegative) {
+    function handlePropertyIncrease(e) {
         let property = e.target.parentNode.id;
-        let i = isNegative ? -1 : 1;
-        console.log(e.target.parentNode.id)
-        if (property === Abi.shields) {
-            const tempCurrShields = currShields + i;
+        if (property === Abi.shields && currShields < maxShields) {
+            const tempCurrShields = currShields + 1;
             setCurrShields(tempCurrShields);
-        } else if (property === Abi.hull) {
-            const tempCurrHull = currHull + i;
+        } else if (property === Abi.hull && currHull < maxHull) {
+            const tempCurrHull = currHull + 1;
+            setCurrHull(tempCurrHull);
+        } else {
+            console.log("Unknown ship property: " + property);
+        }
+    }
+
+    function handlePropertyDecrease(e) {
+        let property = e.target.parentNode.id;
+        if (property === Abi.shields && currShields > 0) {
+            const tempCurrShields = currShields - 1;
+            setCurrShields(tempCurrShields);
+        } else if (property === Abi.hull && currHull > 0) {
+            const tempCurrHull = currHull - 1;
             setCurrHull(tempCurrHull);
         } else {
             console.log("Unknown ship property: " + property);
@@ -25,34 +38,36 @@ export function InnerShip(props) {
     }
 
     return (
-    <div className="row">
-        <div className="col-5">
-            <h3>{Ships[shipId][Abi.name]}</h3>
-            <div>Initiative: {Ships[shipId][Abi.initiative]}</div>
-            <div>Attack: {Ships[shipId][Abi.attack]}</div>
-            <div>Agility: {Ships[shipId][Abi.agility]}</div>
-            <div id="shields">
-                Shields:
-                <Button onClick={e => handleValueChange(e,true)}> - </Button>
-                {currShields}
-                <Button onClick={e => handleValueChange(e, false)}> + </Button>
+        <div className="row">
+            <div className="col-5">
+                <h3>{Ships[shipId][Abi.name]}</h3>
+                <div>Initiative: {Ships[shipId][Abi.initiative]}</div>
+                <div>Attack: {Ships[shipId][Abi.attack]}</div>
+                <div>Agility: {Ships[shipId][Abi.agility]}</div>
+                <div id="shields">
+                    Shields:
+                    <Button onClick={e => handlePropertyDecrease(e, true)}> - </Button>
+                    {currShields}
+                    <Button onClick={e => handlePropertyIncrease(e, false)}> + </Button>
+                </div>
+                <div id="hull">
+                    Hull:
+                    <Button onClick={e => handlePropertyDecrease(e, true)}> - </Button>
+                    {currHull}
+                    <Button onClick={e => handlePropertyIncrease(e, false)}> + </Button>
+                </div>
+                <h4>Select target:</h4>
+                <ol>
+                    {Ships[shipId][Abi.selTarget].map((instruction) =>
+                        <li>{instruction}</li>
+                    )}
+                </ol>
+                <h4>Select and perform action:</h4>
+                <ShipActions shipId={shipId}/>
             </div>
-            <div id="hull">
-                Hull:
-                <Button onClick={e => handleValueChange(e,true)}> - </Button>
-                {currHull}
-                <Button onClick={e => handleValueChange(e, false)}> + </Button>
+            <div className="col-7">
+                IMAGE
             </div>
-            <h4>Select target:</h4>
-            <ol>
-                {Ships[shipId][Abi.selTarget].map((instruction) =>
-                    <li>{instruction}</li>
-                )}
-            </ol>
         </div>
-        <div className="col-7">
-            IMAGE
-        </div>
-    </div>
     )
 }
