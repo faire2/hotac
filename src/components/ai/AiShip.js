d22199ada7a215d79a516a18053e77197ee07f5f
janda <rostislav.janda@fio.cz>
2020-01-14 15:40:28 +0100

    State for shields and hull moved to Shipstats

--- src/components/ai/AiShip.js
+++ src/components/ai/AiShip.js
@@ -7,7 +7,7 @@ import ShipTargetting from "../../data/ShipTargetting";
 import ShipManeuverImages from "../../data/ShipManeuverImages";
 import {PSN, TargetButtons} from "./TargetButtons";
 import Maneuvers from "../../data/Maneuvers";
-import {ShipStats} from "./ShipStats";
+import ShipStats from "./ShipStats";
 
 export function InnerShip(props) {
     const shipType = props.shipType;
@@ -15,44 +15,14 @@ export function InnerShip(props) {
     const maxHull = Ships[shipType][Stats.hull];
     const shipId = props.shipId;
 
-    const [currShields, setCurrShields] = useState(maxShields);
-    const [currHull, setCurrHull] = useState(maxHull);
     const [targetPosition, setTargetPosition] = useState([PSN.FARFRONT]);
-
-    function handlePropertyIncrease(e) {
-        let property = e.target.parentNode.id;
-        if (property === Stats.shields && currShields < maxShields) {
-            const tempCurrShields = currShields + 1;
-            setCurrShields(tempCurrShields);
-        } else if (property === Stats.hull && currHull < maxHull) {
-            const tempCurrHull = currHull + 1;
-            setCurrHull(tempCurrHull);
-        } else if (property !== Stats.hull && property !== Stats.shields) {
-            console.log("Unknown ship property: " + property);
-        }
-    }
-
-    function handlePropertyDecrease(e) {
-        let property = e.target.parentNode.id;
-        if (property === Stats.shields && currShields > 0) {
-            const tempCurrShields = currShields - 1;
-            setCurrShields(tempCurrShields);
-        } else if (property === Stats.hull && currHull > 0) {
-            const tempCurrHull = currHull - 1;
-            setCurrHull(tempCurrHull);
-        } else if (property !== Stats.hull && property !== Stats.shields) {
-            console.log("Unknown ship property: " + property);
-        }
-    }
 
     return (
         <div>
             <h3>{Ships[props.shipType][Stats.name]}</h3>
             <div className="row">
                 <div className="col-6">
-                    <ShipStats shipType={shipType} currShields={currShields} currHull={currHull}
-                               handlePropertyDecrease={handlePropertyDecrease}
-                               handlePropertyIncrease={handlePropertyIncrease}/>
+                    <ShipStats shipType={shipType} maxShields={maxShields} maxHull={maxHull}/>
                     <Button className="btnRemoveShip" variant="danger" size="sm" onClick={e => props.handleShipRemoval(shipId)}>Remove ship</Button>
 
                     <ShipManeuverImages shipType={shipType}/>
