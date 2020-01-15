99555309987de99d7c20856f6cd7ca24d0657b6c
janda <rostislav.janda@fio.cz>
2020-01-15 12:39:23 +0100

    Token ID added to ShipVariables

--- src/components/ai/ShipVariables.js
+++ src/components/ai/ShipVariables.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
-import {Ships, Stats} from "../../data/Ships";
-import {Button} from "react-bootstrap";
+import {Stats} from "../../data/Ships";
+import Select from "react-select";
 
 export default function ShipVariables(props) {
     const maxShields = props.maxShields;
@@ -8,6 +8,21 @@ export default function ShipVariables(props) {
 
     const [currShields, setCurrShields] = useState(maxShields);
     const [currHull, setCurrHull] = useState(maxHull);
+    const [tokenId, setTokenId] = useState();
+
+    const idOptions = [
+        {value: 0, label: 0},
+        {value: 1, label: 1},
+        {value: 2, label: 2},
+        {value: 3, label: 3},
+        {value: 4, label: 4},
+        {value: 5, label: 5},
+        {value: 6, label: 6},
+        {value: 7, label: 7},
+        {value: 8, label: 8},
+        {value: 9, label: 9},
+
+    ];
 
     function handlePropertyIncrease(e) {
         let property = e.target.parentNode.id;
@@ -38,20 +53,26 @@ export default function ShipVariables(props) {
     return (
         <div>
             <div className="row">
-                <div className="col-6">
-                    <p>Shields:</p>
+                <div className="col-4">
+                    <div>ID:</div>
                 </div>
-                <div className="col-6">
-                    <p>Hull:</p>
+                <div className="col-4">
+                    <div>Shields:</div>
+                </div>
+                <div className="col-4">
+                    <div>Hull:</div>
                 </div>
             </div>
             <div className="row">
-                <div id={Stats.shields} className="col-6">
+                <div className="col-4">
+                    <Select options={idOptions} onChange={e => setTokenId(e.value)}/>
+                </div>
+                <div id={Stats.shields} className="col-4">
                     <button className="btn btn-primary btn-sm btn-increment" onClick={e => handlePropertyDecrease(e, true)} size="sm"> - </button>
                     <span className="value"> {currShields} </span>
                     <button className="btn btn-primary btn-sm btn-increment"  onClick={e => handlePropertyIncrease(e, false)} size="sm"> + </button>
                 </div>
-                <div id={Stats.hull} className="col-6">
+                <div id={Stats.hull} className="col-4">
                     <button className="btn btn-primary btn-sm btn-increment"  onClick={e => handlePropertyDecrease(e, true)} size="sm"> - </button>
                     <span className="value"> {currHull} </span>
                     <button className="btn btn-primary btn-sm btn-increment"  onClick={e => handlePropertyIncrease(e, false)} size="sm"> + </button>
