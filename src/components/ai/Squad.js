c5f4979046463c985e375191626842993a3d81d0
janda <rostislav.janda@fio.cz>
2020-01-17 14:52:38 +0100

    Moved squad targetting to standalone component

--- src/components/ai/Squad.js
+++ src/components/ai/Squad.js
@@ -1,24 +1,19 @@
 import React, {useState} from "react";
-import {Button} from "react-bootstrap";
 
 import {Ships, Stats} from "../../data/Ships";
 import SquadActions from "../../data/ShipActions";
-import SquadTargetting from "../../data/ShipTargetting";
-import ShipManeuverImages from "../../data/ShipManeuverImages";
-import {PSN, TargetButtons} from "./TargetButtons";
+import {PSN} from "./TargetButtons";
 import SquadManeuvers from "../../data/Maneuvers";
 import {ShipsVariables} from "./ShipsVariables"
 import {SquadStats} from "./SquadStats";
+import {SquadTargetSelection} from "./SquadTargetting";
+import ShipTargetting from "../../data/ShipTargetting";
 
 export function Squad(props) {
     const shipType = props.shipType;
     const squadId = props.squadId;
 
     const [targetPosition, setTargetPosition] = useState([PSN.FARFRONT]);
-
-
-
-
 
     return (
         <div>
@@ -29,7 +24,7 @@ export function Squad(props) {
                     <ShipsVariables maxHull={Ships[shipType][Stats.hull]} maxShield={Ships[shipType][Stats.shields]}/>
 
                     <h2>Select target:</h2>
-                    <SquadTargetting shipType={shipType}/>
+                    <ShipTargetting shipType={shipType}/>
 
                     <h2>Perform maneuver:</h2>
                     <SquadManeuvers shipType={shipType} position={targetPosition}/>
@@ -38,12 +33,7 @@ export function Squad(props) {
                     <SquadActions shipType={shipType}/>
                 </div>
                 <div className="col-5 position-relative">
-                    <ShipManeuverImages shipType={shipType}/>
-                    <TargetButtons setTargetPosition={setTargetPosition}/>
-                    <div className="d-flex flex-row">
-                        <Button className="btnRemoveShip" variant="danger" size="sm"
-                                onClick={e => props.handleShipRemoval(squadId)}>Remove ship</Button>
-                    </div>
+                    <SquadTargetSelection squadId={props.squadId} shipType={shipType} setTargetPosition={setTargetPosition}/>
                 </div>
             </div>
         </div>
