d07d31729de9f0b02791968127c4a2866ec6f651
janda <rostislav.janda@fio.cz>
2020-01-17 14:24:42 +0100

    Ship components renamed to Squad components

--- src/components/ai/Squad.js
+++ src/components/ai/Squad.js
@@ -2,20 +2,16 @@ import React, {useState} from "react";
 import {Button} from "react-bootstrap";
 
 import {Ships, Stats} from "../../data/Ships";
-import ShipActions from "../../data/ShipActions";
-import ShipTargetting from "../../data/ShipTargetting";
+import SquadActions from "../../data/ShipActions";
+import SquadTargetting from "../../data/ShipTargetting";
 import ShipManeuverImages from "../../data/ShipManeuverImages";
 import {PSN, TargetButtons} from "./TargetButtons";
-import Maneuvers from "../../data/Maneuvers";
-
-import Variables from "./Variables";
+import SquadManeuvers from "../../data/Maneuvers";
 import {ShipsVariables} from "./ShipsVariables"
 import {SquadStats} from "./SquadStats";
 
 export function Squad(props) {
     const shipType = props.shipType;
-    const maxShields = Ships[shipType][Stats.shields];
-    const maxHull = Ships[shipType][Stats.hull];
     const squadId = props.squadId;
 
     const [targetPosition, setTargetPosition] = useState([PSN.FARFRONT]);
@@ -30,19 +26,16 @@ export function Squad(props) {
             <div className="row">
                 <div className="col-6">
                     <SquadStats shipType={shipType}/>
-                    <ShipsVariables maxHull={maxHull} maxShield={maxShields}/>
-
-
-                    <br/>
+                    <ShipsVariables maxHull={Ships[shipType][Stats.hull]} maxShield={Ships[shipType][Stats.shields]}/>
 
                     <h2>Select target:</h2>
-                    <ShipTargetting shipType={shipType}/>
+                    <SquadTargetting shipType={shipType}/>
 
                     <h2>Perform maneuver:</h2>
-                    <Maneuvers shipType={shipType} position={targetPosition}/>
+                    <SquadManeuvers shipType={shipType} position={targetPosition}/>
 
                     <h2>Select and perform action:</h2>
-                    <ShipActions shipType={shipType}/>
+                    <SquadActions shipType={shipType}/>
                 </div>
                 <div className="col-5 position-relative">
                     <ShipManeuverImages shipType={shipType}/>
