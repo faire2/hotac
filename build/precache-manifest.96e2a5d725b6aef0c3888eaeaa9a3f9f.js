76beb37798df656efb7a890e6d2683a2a390b4fd
faire2 <45813901+faire2@users.noreply.github.com>
2020-01-21 08:47:49 +0100

    Build folder

---
+++ build/precache-manifest.96e2a5d725b6aef0c3888eaeaa9a3f9f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9ac9addbf60b477f2e42f8a38ed7ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "403573960a27290e5a87",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b2d89b1df47a1d046831",
+    "url": "/static/css/main.0dc07eae.chunk.css"
+  },
+  {
+    "revision": "403573960a27290e5a87",
+    "url": "/static/js/2.0d5f92bc.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.0d5f92bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2d89b1df47a1d046831",
+    "url": "/static/js/main.05fb88cc.chunk.js"
+  },
+  {
+    "revision": "04fa35e1e9d740378985",
+    "url": "/static/js/runtime-main.61d18be8.js"
+  },
+  {
+    "revision": "63422f315eef8eebad8b2852c7bfac56",
+    "url": "/static/media/tiein.63422f31.png"
+  },
+  {
+    "revision": "f68a954db5cca01a63fc4ac7ca53234a",
+    "url": "/static/media/tieln.f68a954d.png"
+  },
+  {
+    "revision": "f90b0b0c4e6d3936e2626ed1aafb3fee",
+    "url": "/static/media/tiesa.f90b0b0c.png"
+  },
+  {
+    "revision": "81c311ce5e4b36161721cf17ff4013b7",
+    "url": "/static/media/vt49.81c311ce.png"
+  },
+  {
+    "revision": "bc61cbdd6454b2a463bc4513459fd013",
+    "url": "/static/media/xwing-miniatures-ships.bc61cbdd.ttf"
+  },
+  {
+    "revision": "d229957fea8572fd0930c94a9662a0b2",
+    "url": "/static/media/xwing-miniatures.d229957f.ttf"
+  }
+]);
