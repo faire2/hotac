e711473b3e294e47ff49a37939ba8e83997b111f
janda <rostislav.janda@fio.cz>
2020-02-19 15:43:40 +0100

    Hinny's upgrade cards added with a display component

--- src/components/ai/SquadStats.js
+++ src/components/ai/SquadStats.js
@@ -4,28 +4,40 @@ import React from "react";
 export const SquadStats = (props) =>
     <div>
         <div className="row">
-            <div className="col-4">
-                <h3>Initiative:</h3>
+            <div className="col-2">
+                <h3>Init.:</h3>
             </div>
-            <div className="col-4">
+            <div className="col-3">
                 <h3>Attack:</h3>
             </div>
-            <div className="col-4">
+            <div className="col-3">
                 <h3>Agility:</h3>
+            </div>
+            <div className="col-2">
+                <h3>XP:</h3>
+            </div>
+            <div className="col-2">
+                <h3>Level:</h3>
             </div>
         </div>
         <div className="row text-center ship-stats">
-            <div className="col-4">
-                <div>{Ships[props.shipType][Stats.initiative]}</div>
+            <div className="col-2">
+                <div>{props.iniative}</div>
             </div>
-            <div className="col-4">
+            <div className="col-3">
                 {Ships[props.shipType][Stats.attack].map((a, index) =>
                     <span key={index}>
                         {a.attack}{a.damage}
                     </span>)}
             </div>
-            <div className="col-4">
+            <div className="col-3">
                 <div>{Ships[props.shipType][Stats.agility]}</div>
+            </div>
+            <div className="col-2">
+                <div>{props.xp}</div>
+            </div>
+            <div className="col-2">
+                <div>{props.upgradeLevel}</div>
             </div>
         </div>
     </div>;
